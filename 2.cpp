aca29246ffc10a0de1a2dad8aaf7592b08feac94
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-04-03 10:17:11 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n,f=1,i ;
+	cout<<"Enter the number:";
+	cin>>n;
+	if(n<=20)
+	{
+	for(i=1;i<=n;++i)
+	{
+		f*=i;
+}
+	cout<<f;
+	}
+	else{
+	    cout<<"Input is out of range. Enter number upto 20";
+	}
+	if(!cin)
+	{
+		cout<<"Invalid input";
+	}
+
+}
