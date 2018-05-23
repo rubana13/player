96c3c98a01da6d30811eaa5ebe8b440603cc2f28
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-05-23 10:37:23 +0530

    Area of triangle

---
+++ 211.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+int b,h;
+float Area;
+cout<<"Enter the breadth and height of triangle:\n";
+cin>>b>>h;
+if(b,h<=1000000)
+{
+    Area=(b*h)/2;
+    cout<<Area;
+}
+if(!cin)
+{
+    cout<<"Invalid input";
+}
