d8527bb1115229d94dea553c57c1f2d510ca14c5
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-05-23 15:35:55 +0530

    reversing a string

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+ string s;
+
+    int n,i;
+
+ cin>>s;
+
+    n=s.length();    
+    for(i=n;i>=0;i--)
+    {
+
+       cout<<s[i];
+
+    }
+}
